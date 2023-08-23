812dbddac027152f44138f1879b90ac915d20848
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-08-24 00:07:45 +0100

    Update models.py

--- __MAIN__/models.py
+++ __MAIN__/models.py
@@ -1,12 +1,4 @@
 from pydantic import BaseModel
-from datetime import datetime
-
-class User(BaseModel):
-    id: int
-    username: str # at least 4 characters and no spaces
-    email: str
-    name: str
-    password: str # hashed
 
 class UserRequest(BaseModel):
     username: str
@@ -24,41 +16,14 @@ class UserToken(BaseModel):
     username: str
     password: str # unhashed
 
-class Team(BaseModel):
-    id: int
-    name: str # at least 4 characters and no spaces
-    owner_user_id: int
-    active: int
-    individual: int
-
 class TeamRequest(BaseModel):
     name: str
 
 class TeamRequestUpdate(BaseModel):
     name: str
 
-class TeamMember(BaseModel):
-    id: int
-    member_user_id: int
-    team_id: int
-    coach: int
-    confirmed: int
-    declined: int
-
 class TeamMemberRequest(BaseModel):
     member_username: str
-
-class Problem(BaseModel):
-    id: int
-    author_user_id: int
-    name: str
-    statement: str
-    input_statement: str
-    output_statement: str
-    notes: str
-    time_restriction: int
-    memory_restriction: int
-    private: int
 
 class ProblemRequest(BaseModel):
     name: str
@@ -79,14 +44,6 @@ class ProblemRequestUpdate(BaseModel):
     time_restriction: int
     memory_restriction: int
 
-class TestCase(BaseModel):
-    id: int
-    problem_id: int
-    input: str
-    solution: str
-    score: int
-    opened: int
-
 class TestCaseRequest(BaseModel):
     input: str
     solution: str
@@ -98,53 +55,8 @@ class TestCaseRequestUpdate(BaseModel):
     solution: str | None
     score: int | None
 
-class ProblemWithTestCases(Problem):
-    test_cases: list[TestCase]
-
-class Submission(BaseModel):
-    id: int
-    author_user_id: int
-    problem_id: int
-    code: str
-    language_id: int
-    time_sent: datetime
-    checked: int
-    compiled: int
-    compilation_details: str
-    total_verdict_id: int
-
-class SubmissionPublic(BaseModel):
-    id: int
-    author_user_id: int
-    problem_id: int
-    language_id: int
-    time_sent: datetime
-    total_verdict_id: int
-
 class SubmissionRequest(BaseModel):
     problem_id: int
     code: str
     language_name: str
     language_version: str
-
-class SubmissionResult(BaseModel):
-    id: int
-    submission_id: int
-    test_case_id: int
-    verdict_id: int
-    time_taken: int
-    cpu_time_taken: int
-    memory_taken: int
-
-class SubmissionWithResults(Submission):
-    results: list[SubmissionResult]
-
-class Language(BaseModel):
-    id: int
-    name: str
-    version: str
-    supported: int
-
-class Verdict(BaseModel):
-    id: int
-    text: str
