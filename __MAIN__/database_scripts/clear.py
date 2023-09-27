a634bc839f32146e4a3ffde992f6aea0a422eaf0
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-09-27 18:48:12 +0100

    Update clear.py

--- __MAIN__/database_scripts/clear.py
+++ __MAIN__/database_scripts/clear.py
@@ -30,7 +30,7 @@ def clear() -> None:
                 for line in file.read().split(";")[2:]:
                     cursor.execute(line.strip())
             cursor.execute("INSERT INTO problems (author_user_id, name, statement, input_statement, output_statement, notes, time_restriction, memory_restriction, private) VALUES (1, 'Large input', 'Your are given n numbers', 'n = 4 * (10 ^ 6)', 'Nothing', 'Nothing', 10, 1024, 0)")
-            cursor.execute("INSERT INTO test_cases (problem_id, input, solution, score, opened) VALUES (2, %(large_input)s, '', 100, 1)", {'large_input': ''.join(['1\n' for _ in range(4_000_000)])})
+            cursor.execute("INSERT INTO test_cases (problem_id, input, solution, score, opened) VALUES (2, %(large_input)s, '', 100, 0)", {'large_input': ''.join(['1\n' for _ in range(4_000_000)])})
 
 if __name__ == "__main__":
     clear()
